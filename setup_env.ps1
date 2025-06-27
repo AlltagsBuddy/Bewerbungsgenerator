a0f34359da5224e1c07b3c2005ededff03edf234
AlltagsBuddy <info@alltags-buddy.de>
2025-06-27 12:53:06 +0200

    .env angelet

---
+++ setup_env.ps1
@@ -0,0 +1,7 @@
+﻿# Erstellt lokale .env aus Vorlage
+if (-not (Test-Path ".env") -and (Test-Path ".env.template")) {
+    Copy-Item ".env.template" ".env"
+    Write-Host ".env wurde erstellt."
+} else {
+    Write-Host ".env ist vorhanden oder keine Vorlage."
+}
